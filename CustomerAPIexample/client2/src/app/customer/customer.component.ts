41897a2bacff8921857407278836f749337fdcf6
wgambrell <tylergam@yahoo.com>
2018-09-19 01:30:01 -0400

    Customer Example
    From tutorial example online

---
+++ CustomerAPIexample/client2/src/app/customer/customer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer';
+import { CustomerService } from '../customer.service';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+
+  customers: Customer[];
+
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit() {
+    this.getCustomers();
+  }
+
+  getCustomers() {
+    return this.customerService.getCustomers()
+      .subscribe(
+        customers => {
+          console.log(customers);
+          this.customers = customers;
+        }
+      );
+  }
+
+}
