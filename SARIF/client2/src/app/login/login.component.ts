fabad9639617e2f5ccfd4c4a2c596adaeb0dbbc3
Branden <branden.morgan@outlook.com>
2018-09-24 19:39:03 -0400

    #LOGIN-TYLER
    Spell check.

--- SARIF/client2/src/app/login/login.component.ts
+++ SARIF/client2/src/app/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
           this.comp.setSession(this.user.userId, this.user.userName, this.user.userRole);
         }
         else {
-          this.invalidIndicator = 'Loggin failed';
+          this.invalidIndicator = 'Login failed';
         }
       }
     );
