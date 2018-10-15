4eb0292a6d9e7fa36988ce47e18d95eff126e815
Cheyenne Sancho Acebo <32306792+cheyennesancho@users.noreply.github.com>
2018-10-15 15:02:46 -0400

    Add files via upload

---
+++ journal-add/journal-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-add',
+  templateUrl: './journal-add.component.html',
+  styleUrls: ['./journal-add.component.css']
+})
+export class JournalAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ journal-home/journal-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-home',
+  templateUrl: './journal-home.component.html',
+  styleUrls: ['./journal-home.component.css']
+})
+export class JournalHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
